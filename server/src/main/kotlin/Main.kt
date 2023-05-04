9e50da5868759540f7ec3e6f7ca9b47e15288ade
MrKotMatroskin <redyellow3333@gmail.com>
2023-05-05 01:18:57 +0300

    Теперь прога работает
    Костыли при поиске подстроки RES и OK - это, конечно, печально, но ничего другого я сделать не смог

--- server/src/main/kotlin/Main.kt
+++ server/src/main/kotlin/Main.kt
@@ -1,58 +1,50 @@
 import java.net.Socket
-import java.io.InputStream
-import java.io.OutputStream
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
 fun main() {
-    // адрес и порт сервера
+// адрес и порт сервера
     val serverAddress = "npm.mipt.ru"
     val serverPort = 9048
+    Socket(serverAddress, serverPort).use { socket ->
+        val inputStream = socket.getInputStream()
+        val outputStream = socket.getOutputStream()
 
-    // создаем сокет для подключения к серверу
-    val socket = Socket(serverAddress, serverPort)
-    val inputStream: InputStream = socket.getInputStream()
-    val outputStream: OutputStream = socket.getOutputStream()
+        // отправляем команду HELLO
+        println("Отправляем HELLOW")
+        outputStream.write("HELLO\n".toByteArray())
+        outputStream.flush()
 
-    // отправляем команду HELLO
-    print("отправляем команду халлоу")
-    outputStream.write("HELLO\n".toByteArray())
+        //ищем RES, точнее просто проматываем на на 9 символов строку из потока, потому что я не смогу написать поиск подстроки RES
+        println("Ищем RES")
+        for (i in 0 .. 8){
+            inputStream.read().toChar()
+        }
 
-    // получаем ответ с бинарной строкой
-    println("Получаем строку от сервера")
-    val binaryString = waitForBinaryString(inputStream)
-    println(binaryString)
+        //следом за RES считываем длину строки
+        val length = inputStream.read()
 
-    // вычисляем сумму байтов ответа
-    println("Вычисляем сумму")
-    val sum = binaryString.sumOf { it.toInt() }
-    println(sum)
+        val message = ByteArray(length);
+        inputStream.read(message, 0, length)
 
-    // отправляем команду SUM с суммой байтов
-    println("отправляем сумму")
-    outputStream.write("SUM${sum.toString()}\n".toByteArray())
+        //считаем сумму
+        print("Сумма строки: ")
+        val sum = message.toUByteArray().sum()
+        println(sum)
 
-    // получаем ответ OK
-    println("Получаем ответ")
-    val ok = waitForSubstring(inputStream, "OK")
-    println(ok)
+        // отправляем команду SUM с суммой байтов
+        println("Отправляем сумму серверу")
+        outputStream.write("SUM$sum\n".toByteArray())
+        outputStream.flush()
 
-    // закрываем соединение
-    socket.close()
-}
-
-// функция для чтения бинарной строки из потока ввода
-fun waitForBinaryString(inputStream: InputStream): ByteArray {
-    val lengthByte = inputStream.read().toByte()
-    val byteArray = ByteArray(lengthByte.toInt())
-    inputStream.read(byteArray)
-    waitForSubstring(inputStream, "\n") // пропускаем символ перевода строки
-    return byteArray
-}
-
-// функция для чтения строки, содержащей заданную подстроку, из потока ввода
-fun waitForSubstring(inputStream: InputStream, substring: String): String {
-    var buffer = ""
-    while (buffer.endsWith(substring) == true) {
-        buffer += inputStream.read().toChar()
+        //Выводим ОК, аналогично поиску RES
+        for (i in 0 .. 1){
+            inputStream.read()
+        }
+        for (i in 0 .. 1){
+            print(inputStream.read().toChar())
+        }
     }
-    return buffer
 }
